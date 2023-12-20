23dae2041ebd9b6c5b1d0517b213973c59d9de95
kkapi <hZG2fik_j8@mail.ru>
2023-12-21 01:04:46 +0300

    store

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Context } from './main';
+import { observer } from 'mobx-react-lite';
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+const App = observer(() => {
+  const {user} = useContext(Context)
+  console.log(user.isAuth)
 
-export default App
+	return <div>HELLO WORLD</div>
+});
+
+export default App;
---
+++ client/src/store/UserStore.js
@@ -0,0 +1,26 @@
+import { makeAutoObservable } from 'mobx';
+
+export default class UserStore {
+	constructor() {
+		this._isAuth = true;
+		this._userInfo = {};
+
+		makeAutoObservable(this);
+	}
+
+	setIsAuth(newIsAuth) {
+		this._isAuth = newIsAuth;
+	}
+
+	setUser(newUserInfo) {
+		this._userInfo = newUserInfo;
+	}
+
+	get isAuth() {
+		return this._isAuth;
+	}
+
+	get userInfo() {
+		return this._userInfo;
+	}
+}
